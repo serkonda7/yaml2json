530b0020ac7ef7ee228eb98b76274a562123fa85
Lukas Neubert <serkonda7@pm.me>
2022-05-23 22:09:16 +0200

    convert to cli tool

--- src/yaml2json.ts
+++ src/yaml2json.ts
@@ -2,32 +2,52 @@ import * as yaml from 'js-yaml'
 import * as fs from 'fs'
 import * as path from 'path'
 
-const out_dir = path.resolve(__dirname, '..', 'out')
-const yml_paths = [
-	'./language-configuration.yml',
-	'./syntaxes/vba.tmGrammar.yml',
-]
+type Args = {
+	out_dir: string
+	yml_files: string[]
+}
 
-const get_out_path = (inpath: string): string => {
+const parse_cli_args = (args: string[]): Args => {
+	const base_dir = path.dirname(args[0])
+	let res: Args = {
+		out_dir: 'out',
+		yml_files: []
+	}
+	for (let i = 1; i < args.length; i++) {
+		const arg = args[i]
+		if (arg == '-o' || arg == '--out') {
+			i++
+			res.out_dir = args[i]
+			continue
+		}
+		res.yml_files.push(arg)
+	}
+	res.out_dir = path.resolve(base_dir, res.out_dir)
+	return res
+}
+
+const get_final_out_path = (out_dir: string, inpath: string): string => {
 	const out_path = path.join(out_dir, path.basename(inpath))
 	return out_path.replace('.yml', '.json')
 }
 
-const convert_file = (yml_path: string) => {
+const convert_file = (yml_path: string, out_dir: string) => {
 	const yml_txt = fs.readFileSync(yml_path, 'utf8')
 	const obj = yaml.load(yml_txt)
 	const json_txt = JSON.stringify(obj) + '\n'
-	fs.writeFileSync(get_out_path(yml_path), json_txt)
+	fs.writeFileSync(get_final_out_path(out_dir, yml_path), json_txt)
 }
 
 const main = () => {
-	if (!fs.existsSync(out_dir)) {
-		fs.mkdirSync(out_dir)
+	const args = parse_cli_args(process.argv.slice(1))
+
+	if (!fs.existsSync(args.out_dir)) {
+		fs.mkdirSync(args.out_dir)
 	}
 
-	for (const p of yml_paths) {
+	for (const p of args.yml_files) {
 		try {
-			convert_file(p)
+			convert_file(p, args.out_dir)
 		} catch (e) {
 			console.log(e)
 		}
